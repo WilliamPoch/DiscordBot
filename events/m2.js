c0ab2eb92e8af88419cecd63b014684ef57ca3ae
William <pochwilliam42@gmail.com>
2019-09-22 22:08:13 +0700

    make it better

---
+++ events/m2.js
@@ -0,0 +1,11 @@
+module.exports = (client, message) => {
+    if (message.content.startsWith('!kick')) {
+        const kick = require('../commands/kick')
+
+        module.exports = (client, message) => {
+            if (message.content.startsWith('!kick')) {
+                return kick(message)
+            }
+        }
+    }
+}
