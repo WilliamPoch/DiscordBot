0d87485b6a8739b87efa4cc243e6d9bf3caed22b
William <pochwilliam42@gmail.com>
2019-09-22 22:04:43 +0700

    make it better

--- events/message.js
+++ events/message.js
@@ -1,20 +1,17 @@
+
+const kick = require('../commands/kick')
+
 module.exports = (client, message) => {
     if (message.content.startsWith('!kick')) {
-        const kick = require('../commands/kick')
-
-        module.exports = (client, message) => {
-            if (message.content.startsWith('!kick')) {
-                return kick(message)
-            }
-        }
-    }
-    if (message.content.startsWith('!8ball')) {
-        const dice = require('../commands/dice')
-
-        module.exports = (client, message) => {
-            if (message.content.startsWith('!8ball')) {
-                return dice(message)
-            }
-        }
+        return kick(message)
     }
 }
+
+const dice = require('../commands/dice')
+
+module.exports = (client, message) => {
+    if (message.content.startsWith('!8ball')) {
+        return dice(message)
+    }
+}
+
