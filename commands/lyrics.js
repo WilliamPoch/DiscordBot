9209c2ad708e55b17e9e2472e0a5bddcc37631c9
William <pochwilliam42@gmail.com>
2019-09-30 14:40:15 +0700

    make it better

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,11 +1,18 @@
 module.exports = message => {
-    const lyrics = require('4lyrics')
-
+    //const lyrics = require('4lyrics')
+    const solenolyrics = require("solenolyrics");
     var messagesplit = message.content.split(" ");
     messagesplit.splice(0, 1);
-    messagesplit.join(" ");
+    var string = messagesplit.join(" ");
 
-
+    async function f() {
+        var lyrics = await solenolyrics.requestLyricsFor(string);
+        return (lyrics);
+    };
+    f().then(a => message.reply(a, { split: true })).catch(function catchError(error) { 
+        console.log(error)
+        message.channel.send(error)
+    });
 
     //lyrics.lyricslive.getURL(messagesplit)
     //    .then(r => lyrics.lyricslive.getLyrics(r))
@@ -21,18 +28,17 @@ module.exports = message => {
     //    .then(r => lyrics.azlyrics.getLyrics(r))
     //    .then(a => message.reply("\n" + a, { split: true })).catch(console.error)
     //    .catch(console.error);
-
-    try {
-        lyrics.lyricscom.getURL(messagesplit)
-            .then(r => lyrics.lyricscom.getLyrics(r)).catch(console.error)
-            .then(a => {
-                message.reply("\n" + a, { split: true });   
-            }).catch (console.error)
-            .catch (function onError(error) { console.log(error); });
-    } catch (e) {
-        console.log(e.stack);
-        message.channel.send(e);
-    };
+    //try {
+    //    lyrics.musixmatch.getURL(string)
+    //        .then(r => lyrics.musixmatch.getLyrics(r)).catch(console.error)
+    //        .then(a => {
+    //            message.reply("\n" + a, { split: true });   
+    //        }).catch (console.error)
+    //        .catch (function onError(error) { console.log(error); });
+    //} catch (e) {
+    //    console.log(e.stack);
+    //    message.channel.send(e);
+    //};
 
 
 }
