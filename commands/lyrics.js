e9a8869975b72a49e1259f073577d7ab3f103bd0
William <pochwilliam42@gmail.com>
2019-09-30 10:29:58 +0700

    make it better

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -5,23 +5,34 @@ module.exports = message => {
     messagesplit.splice(0, 1);
     messagesplit.join("");
 
+
+
     //lyrics.lyricslive.getURL(messagesplit)
     //    .then(r => lyrics.lyricslive.getLyrics(r))
     //    .then(a => message.reply("\n" + a)).catch(console.error)
     //    .catch(console.error);
 
-    lyrics.musixmatch.getURL(messagesplit)
-        .then(r => lyrics.musixmatch.getLyrics(r))
-        .then(a => message.reply("\n" + a)).catch(console.error)
-        .catch(console.error);
+    //lyrics.musixmatch.getURL(messagesplit)
+    //    .then(r => lyrics.musixmatch.getLyrics(r))
+    //    .then(a => message.reply("\n" + a)).catch(console.error)
+    //    .catch(console.error);
 
     //lyrics.lyricscom.getURL(messagesplit)
     //    .then(r => lyrics.lyricscom.getLyrics(r))
     //    .then(a => message.reply("\n" + a)).catch(console.error)
     //    .catch(console.error);
 
-    //lyrics.azlyrics.getURL(messagesplit)
-    //    .then(r => lyrics.azlyrics.getLyrics(r)).catch(console.error)
-    //    .then(a => message.reply("\n" + a)).catch(console.error)
+    try {
+        lyrics.azlyrics.getURL(messagesplit)
+            .then(r => lyrics.azlyrics.getLyrics(r)).catch(console.error)
+            .then(a => {
+                message.reply("\n" + a, { split: true });   
+            }).catch (console.error)
+            .catch (function onError(error) { console.log(error); });
+    } catch (e) {
+        console.log(e.stack);
+        message.channel.send(e);
+    };
+
 
 }
