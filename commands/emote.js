8b65fc67a9bb7c2a25ffa09a762e80ed65a2cde1
William <pochwilliam42@gmail.com>
2019-10-04 14:46:28 +0700

    make it better

--- commands/emote.js
+++ commands/emote.js
@@ -3,12 +3,12 @@ module.exports = message => {
     var emotes = ['feelsbadman'];
     var msg = message.content.toLowerCase();
 
-    if (chance > 1) {
+    if (chance > -1) {
         for (i = 0; i < emotes.length; i++) {
             if (message.content.includes(emotes[i])) {
                 message.channel.send({ files: ['./img/' + emotes[i] + '.png'] });
             } else if (msg.includes("YA")) {
-                message.channel.send({ files: ['./img/yeet.mp4]'] });
+                message.channel.send({ files: ['./img/yeet.gif]'] });
             } else if (msg.includes("f")) {
                 message.channel.send('F');
             }
