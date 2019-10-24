bd8574ad432b270ef980c3d7fe1482afdf7aff62
William <pochwilliam42@gmail.com>
2019-10-24 14:14:14 +0700

    fix

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,8 @@
 module.exports = client => {
+	const fs = require('fs');
+	let filename = './logs/log' + date() + '.txt';
+    let CreateFiles = fs.createWriteStream(filename, {
+        flags: 'a'
+    })
     console.log(`Logged in as ${client.user.tag}!`)
 }
