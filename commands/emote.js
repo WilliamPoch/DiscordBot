23b5fdd0a47134ec0017a3668f9178f5f82a3030
William <pochwilliam42@gmail.com>
2019-10-24 13:07:15 +0700

    fixes

--- commands/emote.js
+++ commands/emote.js
@@ -1,20 +1,19 @@
 module.exports = message => {
-    var chance = Math.floor(Math.random() * 3 + 1);
     var emotes = ['feelsbadman'];
     var msg = message.content.toLowerCase();
 
-    if (chance > 2) {
-        for (i = 0; i < emotes.length; i++) {
-            if (message.content.includes(emotes[i])) {
-                message.channel.send({ files: ['./img/' + emotes[i] + '.png'] });
-            } else if (msg.includes("YA")) {
-                message.channel.send({ files: ['./img/yeet.gif]'] });
-            } else if (msg.includes("f")) {
-                message.channel.send('F');
-            }
-        }
-       
-    }
+
+	for (i = 0; i < emotes.length; i++) {
+		if (message.content.includes(emotes[i])) {
+			message.channel.send({ files: ['./img/' + emotes[i] + '.png'] });
+		} else if (msg.includes("YA")) {
+			message.channel.send({ files: ['./img/yeet.gif]'] });
+		} else if (msg.includes("f")) {
+			message.channel.send('F');
+		}
+	}
+      
+    
     
 
 }
