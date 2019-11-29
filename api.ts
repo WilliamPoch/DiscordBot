e74de7860718f402a16d6cee6c9686140a739540
William <55649385+WilliamPoch@users.noreply.github.com>
2019-11-29 18:16:56 +0700

    Delete api.ts

--- api.ts
+++
@@ -1,7 +0,0 @@
-import * as Discord from "discord.js";
-
-export interface IBotCommands {
-    help(): string;
-    isThisCommand(command: string): boolean;
-    runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void
-}
